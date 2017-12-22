53f6810f267fe47e9dd5cc92022e751a481f8b31
Francisco Souza <francisco.souza@ifnet-for.ifactory.com.br>
2017-12-22 11:13:39 -0300

    Added some reducers and actions to show how to use redux with react

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(acc) {
+    return [
+        { title: 'JavaScript: The Good Parts', pages: 101 },
+        { title: 'Harry Potter', pages: 200 },
+        { title: 'The Dark Tower', pages: 300 },
+        { title: 'Eloquent Ruby' , pages: 1}
+    ];
+}
